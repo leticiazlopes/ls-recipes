16f5564a062edd3a8320b14feddc9dba8e6f06fc
leticiazlopes <leticiazlzlima@gmail.com>
2025-05-16 11:50:59 -0300

    :wrench: chore: add nuxt version

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-05-15',
+  future: {
+  compatibilityVersion: 4,
+  },
   devtools: { enabled: true }
-})
+});
