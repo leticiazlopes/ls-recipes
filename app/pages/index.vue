27103d97a05c4b744a9b89393283c4f64b9a0b20
leticiazlopes <leticiazlzlima@gmail.com>
2025-05-19 11:56:21 -0300

    feat: initial base components

---
+++ app/pages/index.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <main>
+        <section class="bg-[#f1f1f1]">
+      <div class="container flex flex-col lg:flex-row items-center py-20 gap-10">
+        <div class="flex-1 order-2 lg:order-1 text-center lg:text-left">
+          <h1 class="text-4xl lg:text-6xl font-extrabold mb-6 text-balance">
+            Master the Kitchen with Ease: Unleash Your Inner Chef Today!
+          </h1>
+          <p class="text-xl lg:text-2xl mb-8 text-balance">
+            Discover recipes helping you to find the easiest way to cook.
+          </p>
+          <UButton to="#recipes" label="Browse Recipes" />
+        </div>
+        <div class="flex-1 order-1 lg:order-2">
+          <NuxtImg
+            sizes="xs:100vw sm:667px"
+            src="/brownies_tradicional.jpeg"
+            format="webp"
+            densities="x1"
+            alt=""
+          />
+        </div>
+      </div>
+    </section>
+    
+    </main>
+</template>
+
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,51 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  compatibilityDate: "2025-05-19",
+
   future: {
-  compatibilityVersion: 4,
+    compatibilityVersion: 4,
   },
-  devtools: { enabled: true }
+
+  experimental: {
+  scanPageMeta: 'after-resolve',
+  sharedPrerenderData: false,
+  compileTemplate: true,
+  resetAsyncDataToUndefined: true,
+  templateUtils: true,
+  relativeWatchPaths: true,
+  normalizeComponentNames: false,
+  spaLoadingTemplateLocation: 'within',
+  parseErrorData: false,
+  pendingWhenIdle: true,
+  alwaysRunFetchOnKeyChange: true,
+  defaults: {
+    useAsyncData: {
+      deep: true
+    }
+  }
+},
+
+  features: {
+    inlineStyles: true
+  },
+
+  unhead: {
+    renderSSRHeadOptions: {
+      omitLineBreaks: false
+    }
+  },
+
+  devtools: { enabled: true },
+  modules: [
+    '@nuxtjs/tailwindcss',
+    '@nuxtjs/google-fonts',
+    '@nuxt/icon',
+    '@nuxt/image'
+  ],
+
+  googleFonts : {
+    families: {
+      Montserrat: true,
+    },
+  },
 });
